2db825ed696e547ba7e3525f711d9b31ba66e80f
Laura Tristan <lauratrpolo@gmail.com>
2019-12-12 19:05:48 +0100

    receiving all data from server and customize marker color

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,53 +10,68 @@ const mapStyles = {
   height: '90%'
 };
 
+const colores = {
+  verde: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+  rojo: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+  azul: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+}
+
 export class MapContainer extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      teams: [
-        {team: "rojo", latitude: 41.3846877, longitude: 2.1800151999999997},
-      ]
+      jugadores: []
     }
 
     /*requestFromControlCenter: Al cargar la página, le pedimos al servidor que nos mande las coordenadas*/
-    socket.emit("requestCoordenadasFromCC")
+    socket.emit("requestCoordenadasFromCC", (data) => {
+      const parseData = JSON.parse(data)
+      this.setState({
+        jugadores: parseData
+      })
+    })
     
     socket.on("coordenadasFromServer", data => {
-        let noEncontrado = true
-        let equipos = this.state.teams
-        equipos.map((item, key)=>{
-          if(item.team == data.team) {
-            equipos[key].latitude = data.latitude
-            equipos[key].longitude = data.longitude
-            noEncontrado = false
-            console.log('El equipo '+data.team+' se está moviendo')
-            console.log(data)
-          }
-        })
-        if (noEncontrado) {
-          console.log('El equipo '+data.team+' ha sido añadido al mapa')
-          equipos.push(data)
-        }
-        this.setState({
-          teams: equipos
-        })
+      this.actualizarCoordenadasState(data)
+    })
+  }
+
+  actualizarCoordenadasState = (data) => {
+    let noEncontrado = true
+    let equipos = this.state.jugadores
+    equipos.map((item, key)=>{
+      if(item.equipo == data.equipo) {
+        equipos[key].latitude = data.latitude
+        equipos[key].longitude = data.longitude
+        noEncontrado = false
+        console.log('El equipo '+data.equipo+' se está moviendo')
+        console.log(data)
       }
-    )
+    })
+    if (noEncontrado) {
+      console.log('El equipo '+data.equipo+' ha sido añadido al mapa')
+      equipos.push(data)
+    }
+    this.setState({
+      jugadores: equipos
+    })
   }
 
 
   displayMarkers = () => {
-    return this.state.teams.map((team, index) => {
+    return this.state.jugadores.map((jugador, index) => {
+      let iconUrl = colores[jugador.equipo] ? colores[jugador.equipo] : "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
       return <Marker key={index} id={index} position={
         {
-          lat: team.latitude,
-          lng: team.longitude
+          lat: jugador.latitude,
+          lng: jugador.longitude
         }
       }
+      title={jugador.jugador}
+      //name={'nombre'}
       icon={
-        {url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"}
+        {url: iconUrl}
       }
       />
     })
