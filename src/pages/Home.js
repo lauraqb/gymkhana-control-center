a6867708a6b5a269fd821312b9fb37230a01d660
unknown <lauratrpolo@gmail.com>
2019-12-26 21:33:21 +0100

    check si hay fichero .env o si server esta funcionando

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,31 @@ import React from 'react';
 import PartidasContainer from '../components/Partidas'
 import socketIOClient from "socket.io-client";
 
-const endpoint = process.env.REACT_APP_SERVER_ENDPOINT
-const socket = socketIOClient(endpoint);
+const serverEndpoint = process.env.REACT_APP_SERVER_ENDPOINT
+const socket = socketIOClient(serverEndpoint);
 
 class Home extends React.Component {
-    // constructor(props) {
-    // }
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null,
+            envMissing: false
+          };
+    }
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        if (serverEndpoint) fetch(serverEndpoint).catch(error => {debugger});
+        else this.setState({ envMissing: true });
+      }
+    
     render() {
+        const { envMissing, error } = this.state;
+        if (envMissing) {
+            return <p>Falta el fichero .env</p>;
+        }
+        else if (error) {
+            return <p>Error del servidor: {error.message}</p>;
+        }
         return <PartidasContainer socket={socket}/>
     }
 }
