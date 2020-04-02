1bf501be968c3ff69fbdfb1841cc95e13955136e
Laura Tristan <lauratrpolo@gmail.com>
2020-04-02 22:18:07 +0200

    cambios

--- src/pages/Challenges.js
+++ src/pages/Challenges.js
@@ -10,23 +10,28 @@ class GamePanel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            gameName: null,
             challenges: null,
             error: null
         }
         this.id = this.props.match.params.id
-        this.getChallengesJson()
+        this.getGameData()
         this.onChange = this.onChange.bind(this)
         this.updateJson = this.updateJson.bind(this)
     }
+
 
     onChange = (e) => {
         if(e && e.jsObject) {
             this.setState({ challenges: e.jsObject })
         }
-        
     }
 
     updateJson = () => {
+        this.setState({ error: null })
+        var r = window.confirm("¿Confirmas guardar los cambios?");
+        if (!r) return
+
         axios.put(endpoint+"/games/"+this.id+"/updateChallenges", this.state.challenges)
         .then(res => {
             if (res.data.error) {
@@ -40,12 +45,12 @@ class GamePanel extends React.Component {
         .catch(error => this.setState({ error: error.message }));
     }
 
-    getChallengesJson = () => {
+    getGameData = () => {
         axios.get(endpoint+"/games/"+this.id+"/challenges")
         .then(res => {
-            console.log(res.data.info);
             this.setState({
                 error: null,
+                gameName: res.data.name,
                 challenges: res.data.info
             })
         })
@@ -56,20 +61,21 @@ class GamePanel extends React.Component {
 
         // if(!this.state.challenges) return <div>No challenges</div>
         return ( 
-            <div>
+            <React.Fragment>
                 {this.state.error && <div>{this.state.error}</div>}
+                <h3>{this.state.gameName}</h3>
                 <JSONInput
                     id          = 'a_unique_id'
                     placeholder = { this.state.challenges }
                     locale      = { locale }
                     width       = '100%'
-                    height      = 'calc(100vh - 50px)'
+                    height      = 'calc(100vh - 80px)'
                     min-height  = '100px'
                     onChange    = {this.onChange}
                     waitAfterKeyPress	= '10000'
                 />
                 <Button onClick={this.updateJson}>Guardar Cambios</Button>
-            </div>
+            </React.Fragment>
         )
     }
 }
